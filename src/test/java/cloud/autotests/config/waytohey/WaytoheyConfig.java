b5a5577c9961bcfd96c1af6c342d4eae03be9b2b
Valentina <15valentinka@gmail.com>
2021-12-27 00:32:36 +0500

    add images

--- src/test/java/cloud/autotests/config/waytohey/WaytoheyConfig.java
+++ src/test/java/cloud/autotests/config/waytohey/WaytoheyConfig.java
@@ -10,10 +10,10 @@ import org.aeonbits.owner.Config;
 
 public interface WaytoheyConfig extends Config {
 
-    @DefaultValue("valentina71551")
+
     String login_correct();
 
-    @DefaultValue("r347b7")
+
     String password_correct();
 
     @DefaultValue("dfg@ml.rootkid.ru")
