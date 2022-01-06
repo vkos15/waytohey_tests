241dd2fd61c0398c9bf2c7add93b78cce67e3a6f
Valentina <15valentinka@gmail.com>
2022-01-07 03:49:06 +0500

    add allure steps

--- src/test/java/cloud/autotests/pages/MessagePage.java
+++ src/test/java/cloud/autotests/pages/MessagePage.java
@@ -1,5 +1,7 @@
 package cloud.autotests.pages;
 
+import io.qameta.allure.Step;
+
 import static com.codeborne.selenide.CollectionCondition.size;
 import static com.codeborne.selenide.Condition.attributeMatching;
 import static com.codeborne.selenide.Condition.text;
@@ -7,24 +9,29 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 
 public class MessagePage {
+
+    @Step("Открываем окно сообщений")
     public void openMessageWindow() {
         $("#mess_link").scrollIntoView(true).click();
     }
 
-
+    @Step("Поиск в списке контактов")
     public void searchContacts(String searchQuery) {
         $("#search_contacts input[name='search']").setValue(searchQuery)
                 .pressEnter();
     }
 
+    @Step("Проверяем результат поиска в списке контактов")
     public void checkResultsOfSearch(String searchQuery) {
         $("#contact_list").shouldHave(text(searchQuery));
     }
 
+    @Step("Проверяем количество контактов в результатах поиска")
     public void checkCountOfUsers(int count) {
         $$("#contact_list div.contact_el").shouldHave(size(count));
     }
 
+    @Step("Проверяем, что открылся диалог с заданным пользователем")
     public void checkThatChanOpen(String userLogin) {
         //проверили что в заголовке ссылка на юзера, с которым открыт диалог
         $("#chat_top .top_title").shouldHave(attributeMatching("href", ".*" + userLogin + ".*"));
